32def54f886209d8226ca3e8f0f16fd14cd3834d
npacariz <nikolapacariz@gmail.com>
2018-09-06 18:35:50 +0200

    added assets folder

--- src/main.js
+++ src/main.js
@@ -5,6 +5,9 @@ import VueOnsen from 'vue-onsenui';
 // Onsen UI Styling and Icons
 require('onsenui/css-components-src/src/onsen-css-components.css');
 require('onsenui/css/onsenui.css');
+require('onsenui/css/onsenui.css');
+require('./assets/onsen-css-components.css')
+require('./assets/theme.css')
 
 import App from './App.vue';
 
