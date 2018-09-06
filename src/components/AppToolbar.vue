3246ceadd2f20b5df5eee0a01a4c2c16b7957fc3
npacariz <nikolapacariz@gmail.com>
2018-09-06 18:44:36 +0200

    added apptoolbar

---
+++ src/components/AppToolbar.vue
@@ -0,0 +1,32 @@
+<template>
+    <v-ons-page>
+      <v-ons-toolbar>
+        <div class="right">
+          <v-ons-toolbar-button>
+              <slot name="back"></slot>
+          </v-ons-toolbar-button>
+        </div>
+        <div class="center">{{ title }}</div>
+        <div class="right">
+          <v-ons-toolbar-button>
+                <slot name="forward"></slot>
+          </v-ons-toolbar-button>
+        </div>
+      </v-ons-toolbar>
+    </v-ons-page>
+</template>
+<script>
+  export default{
+    props: {
+        title: {
+            type: String,
+            required: true
+        }
+    },
+    data() {
+      return {
+        title: 'AppToolbar'
+      };
+    },
+  };
+</script>
